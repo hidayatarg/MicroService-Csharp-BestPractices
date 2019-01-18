c1776b661ad79bc346d07eee75e15191af3b709d
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-18 17:42:11 +0300

    Created all Layers

---
+++ DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/NinjectValidationFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Ninject;
+using Ninject.Modules;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class NinjectValidationFactory:ValidatorFactoryBase
+    {
+        private IKernel _kernel;
+
+        public NinjectValidationFactory(INinjectModule module)
+        {
+            _kernel =new StandardKernel(module);
+        }
+
+        public override IValidator CreateInstance(Type validatorType)
+        {
+            return (IValidator)_kernel.TryGet(validatorType);
+        }
+    }
+}
---
+++ MemberRegistration.MVCWebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MemberRegistration.MVCWebUI.MvcApplication" Language="C#" %>
