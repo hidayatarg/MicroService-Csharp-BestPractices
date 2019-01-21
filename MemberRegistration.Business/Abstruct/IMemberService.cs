c419c9f7cf1644e97127ab18974a70898264ae47
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-21 11:46:03 +0300

    Business Layer Implemented

---
+++ MemberRegistration.Business/Abstruct/IMemberService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.Business.Abstruct
+{
+    public interface IMemberService
+    {
+        void Add(Member member);
+    }
+}
---
+++ MemberRegistration.Business/Concrete/MemberManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberRegistration.Business.Abstruct;
+using MemberRegistration.DataAccess.Abstruct;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.Business.Concrete
+{
+    public class MemberManager:IMemberService
+    {
+        // Need access to DataAccess Layer
+        private IMemberDal _memberDal;
+
+        public MemberManager(IMemberDal memberDal)
+        {
+            _memberDal = memberDal;
+        }
+        public void Add(Member member)
+        {
+           // Islem(Actions)
+           _memberDal.Add(member);
+        }
+    }
+}
