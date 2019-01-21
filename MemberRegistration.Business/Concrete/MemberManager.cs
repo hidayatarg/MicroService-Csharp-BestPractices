2d5c6648edb4059d01912e63cdd22919b5fd02bb
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-21 12:22:08 +0300

    MicrosService Architecture Implementation (Governoment Id Verification Microservice)

--- MemberRegistration.Business/Concrete/MemberManager.cs
+++ MemberRegistration.Business/Concrete/MemberManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MemberRegistration.Business.Abstruct;
+using MemberRegistration.Business.ServiceAdapters;
 using MemberRegistration.DataAccess.Abstruct;
 using MemberRegistration.Entities.Concrete;
 
@@ -13,14 +14,23 @@ namespace MemberRegistration.Business.Concrete
     {
         // Need access to DataAccess Layer
         private IMemberDal _memberDal;
+        private IKpsService _kpsService;
 
-        public MemberManager(IMemberDal memberDal)
+        public MemberManager(IMemberDal memberDal, IKpsService kpsService)
         {
             _memberDal = memberDal;
+            _kpsService = kpsService;
         }
         public void Add(Member member)
         {
-           // Islem(Actions)
+            // Islem(Actions)
+
+            // Check the member from kimlik dogrula 
+            if (_kpsService.ValidateUser(member) == false)
+            {
+                throw new Exception("Kullanici dogrulamasi gecerli degil");
+            }
+
            _memberDal.Add(member);
         }
     }
---
+++ MemberRegistration.Business/ServiceAdapters/KpsServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberRegistration.Business.KpsServiceReference;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.Business.ServiceAdapters
+{
+    public class KpsServiceAdapter:IKpsService
+    {
+        public bool ValidateUser(Member member)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula( Convert.ToInt64(member.TcNo),member.FirstName.ToUpper(),
+                member.LastName.ToUpper(), member.DateofBirth.Year);
+
+        }
+    }
+}
