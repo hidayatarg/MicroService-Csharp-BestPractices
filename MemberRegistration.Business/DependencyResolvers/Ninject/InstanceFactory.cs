5a19c402f075601cb5a367faeeb75ac0e750cdca
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-21 14:10:07 +0300

    IoCContainer - Instance Factory

---
+++ MemberRegistration.Business/DependencyResolvers/Ninject/InstanceFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Utilities.Mappings;
+using Ninject;
+
+namespace MemberRegistration.Business.DependencyResolvers.Ninject
+{
+    // For integration of WinForm /WP
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var kernal = new StandardKernel(new BusinessModule());
+            return kernal.Get<T>();
+        }
+    }
+}
