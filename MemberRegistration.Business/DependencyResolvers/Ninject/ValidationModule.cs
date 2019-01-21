e8b3595a0cae2fb5772ca48cabeadc3935696049
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-21 14:05:59 +0300

    IoCContainer - Validation Module Implementation

---
+++ MemberRegistration.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MemberRegistration.Business.ValidationRules.FluentValidation;
+using MemberRegistration.Entities.Concrete;
+using Ninject.Modules;
+
+namespace MemberRegistration.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {// Validation according to member
+            Bind<IValidator<Member>>().To<MemberValidator>().InSingletonScope();
+        }
+    }
+}
